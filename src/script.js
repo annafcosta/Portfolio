35adb68fb6407202027570f800b4d7060616814b
Anna Luísa <annalf1401@gmail.com>
2024-11-15 15:11:00 -0300

    Functions to enable and disable

--- src/script.js
+++ src/script.js
@@ -11,3 +11,35 @@ document.addEventListener("scroll", function () {
         socialMedia.classList.add("hidden");
     }
 });
+
+// Functions to enable and disable project information
+function showPopup(targetId) {
+    const popup = document.getElementById(targetId);
+    if (popup) {
+        popup.classList.remove("hidden"); // Show the popup
+    }
+}
+
+function hidePopup(targetId) {
+    const popup = document.getElementById(targetId);
+    if (popup) {
+        popup.classList.add("hidden");
+    }
+}
+
+const openButtons = document.querySelectorAll(".read-more");
+openButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        showPopup(targetId); // Call the function to show the popup
+    });
+});
+
+const closeButtons = document.querySelectorAll(".close");
+closeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        hidePopup(targetId);
+    });
+});
+
